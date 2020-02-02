0f7868866081d4e90a4ec923370c4f54e847cd70
gthe1andonly <54093146+gthe1andonly@users.noreply.github.com>
2020-02-02 03:11:11 -0500

    Update and rename git pull temp to quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -6,16 +6,6 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# from here
-import pyowm
-
-owm = pyowm.OWM('2fcc348ab08d9beb8db26c2a1285c5ff')
-report = owm.weather_at_place('Laurel, US')
-w = report.get_weather()
-temp = w.get_temperature('fahrenheit')['temp']
-print(f'The temperature : {temp}')
-# to here works 
-
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
